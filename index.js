2c5117e06efae9c509a767184ac7fa486b1f618d
Dan Spagnoli <dan.spagnoli@vh1.com>
2017-11-18 14:35:25 -0500

    update

--- index.js
+++ index.js
@@ -8,14 +8,16 @@ require('datejs');
 var http = require('http');
 var app = express();
 var port = process.env.PORT || 8080;
-const ddd = {Id:3064646,Date:'2017-11-16T20:00:00',Venue:{Id:151240,Name:'Jays House',Address:'149 Westchester Avenue',City:'New York',State:'New York',StateCode:'NY',Country:'US',CountryCode:'US',ZipCode:'10573',Url:'#',Latitude:41.001397,Longitude:-73.6654978},Artists:[{Id:65944,Name:'Jay'}],TicketUrl:'#'};
-const ddd2 = {Id:3064646,Date:'2017-11-16T20:00:00',Venue:{Id:151240,Name:'Jays House 2 The Electric one In the Sky',Address:'149 Westchester Avenue',City:'New York',State:'New York',StateCode:'NY',Country:'US',CountryCode:'US',ZipCode:'10573',Url:'#',Latitude:41.001397,Longitude:-73.6654978},Artists:[{Id:65944,Name:'Jay'}],TicketUrl:'#'};
+const ddd = { Id: 3064646, Date: '2020-01-01T20:00:00', Venue: { Id: 151240, Name: 'Jays House', Address: '69 West Avenue', City: 'New York', State: 'New York', StateCode: 'NY', Country: 'US', CountryCode: 'US', ZipCode: '10573', Url: '#', Latitude: 41.001397, Longitude: -73.6654978 }, Artists: [{ Id: 69, Name: 'Jay' }], TicketUrl: '#' };
+const ddd2 = { Id: 3064646, Date: '2021-01-01T20:00:00', Venue: { Id: 151240, Name: 'Jays House 2 The Bungalow', Address: '96 East Avenue', City: 'New York', State: 'New York', StateCode: 'NY', Country: 'US', CountryCode: 'US', ZipCode: '10573', Url: '#', Latitude: 41.001397, Longitude: -73.6654978 }, Artists: [{ Id: 65944, Name: 'Jay' }], TicketUrl: '#' };
 
 var p = 0;
 var data = [];
 var compression = require('compression');
 app.use(compression());
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(markoExpress());
+
 function getData(page) {
     console.log("Getting Concert Info for page " + page)
 
@@ -29,20 +31,17 @@ function getData(page) {
         });
 
         res.on('end', function() {
-        	try{
-            var result = JSON.parse(body);
-            if(p === 0)
-            {
-                data = [];
-            }
-            }
-            catch(e){
-            	p=11;
-            	var result = {};
-                if(data.length < 1){
-            	result.Events = [ddd, ddd, ddd2, ddd, ddd, ddd, ddd2];
+            try {
+                var result = JSON.parse(body);
+                if (p === 0) {
+                    data = [];
                 }
-                else{
+            } catch (e) {
+                p = 11;
+                var result = {};
+                if (data.length < 1) {
+                    result.Events = [ddd, ddd, ddd2, ddd, ddd, ddd, ddd2];
+                } else {
                     result.Events = [];
                 }
             }
@@ -51,7 +50,7 @@ function getData(page) {
                 data.push(result.Events[i]);
             }
             p++;
-            if (p <= 10) {
+            if (p <= 25) {
                 getData(p);
             } else {
                 p = 0;
@@ -66,11 +65,8 @@ function getData(page) {
 
 }
 getData(p);
-setInterval(getData, 7200000, p);
-
-app.use(markoExpress()); //enable res.marko(template, data)
-
-
+var dayInMilliseconds = 1000 * 60 * 60 * 24;
+setInterval(getData, dayInMilliseconds, p);
 app.get('/', function(req, res) {
     res.marko(template, {
         events: data
