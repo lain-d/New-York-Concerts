bb8a439c554f640962738854cc3d6a8f81af50f3
dan2600 <daniel.spagnoli@gmail.com>
2017-11-25 11:37:18 -0500

    fixed API key

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.config = {
-	"APIkey": "45gxnvd2mm8ysvc72sxkzcsrv", //please use your own API Key!!!!!!!
+	"APIkey": "45gxnvd2mm8ysvc72xkzcsrv", //please use your own API Key!!!!!!!
 	"zipCode": "10010",
 	"radius":"50",
 	"redisURL": process.env.REDIS_URL
