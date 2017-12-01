2acfd948c763131a825c07da77881fafdf47400f
Dan Spagnoli <Dan.Spagnoli@vh1.com>
2017-12-01 11:30:58 -0500

    async font load for speed

--- index.marko.js
+++ index.marko.js
@@ -37,9 +37,9 @@ function render(input, out, __component, component, state) {
     marko_escapeXml(input.city) +
     " Showlist</title><meta name=\"description\" content=\"A LoFi list of upcoming " +
     marko_escapeXmlAttr(input.city) +
-    " shows, powered by JamBase\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0\"><link href=\"https://fonts.googleapis.com/css?family=Nunito:400,700,900\" rel=\"stylesheet\"> ");
+    " shows, powered by JamBase\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0\">");
 
-  lasso_head_tag({}, out, __component, "8");
+  lasso_head_tag({}, out, __component, "7");
 
   out.w("</head><body>");
 
@@ -65,19 +65,19 @@ function render(input, out, __component, component, state) {
       renderBody: function renderBody(out, rp) {
         app_showtable_tag({
             events: rp.data
-          }, out, __component, "19");
+          }, out, __component, "18");
       }
-    }, out, __component, "18");
+    }, out, __component, "17");
 
-  out.w("<h4 style=\"font-size: 65%; padding-top:10px\">powered by <b><a style=\"text-weight:700\" href=\"http://www.JamBase.com\">JamBase™</a></b></h4></div>");
+  out.w("<h4 style=\"font-size: 65%; padding-top:10px\">powered by <b><a style=\"text-weight:700\" href=\"http://www.JamBase.com\">JamBase™</a></b></h4></div><script src=\"https://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js\"></script><script>\n WebFont.load({\n    google: {\n      families: ['Nunito:400,700,900']\n    }\n  });\n</script>");
 
-  lasso_body_tag({}, out, __component, "23");
+  lasso_body_tag({}, out, __component, "24");
 
-  browser_refresh_tag({}, out, __component, "24");
+  browser_refresh_tag({}, out, __component, "25");
 
   init_components_tag({}, out);
 
-  await_reorderer_tag({}, out, __component, "25");
+  await_reorderer_tag({}, out, __component, "26");
 
   out.w("</body></html>");
 }
@@ -93,7 +93,7 @@ marko_template.meta = {
     deps: [
       {
           type: "css",
-          code: "body,\nhtml {\n    font-family: 'Nunito', 'Verdana', sans-serif\n}\na {\n    color: black;\n    text-decoration: none\n}\n.th {\n    font-size: 85%;\n    box-sizing: border-box;\n    word-wrap: break-word;\n    display: table-cell;\n    vertical-align: middle;\n    width: 25%;\n    border: solid 1px #EFEFEF;\n    page-break-before: auto;\n    page-break-after: auto;\n    page-break-inside: avoid\n}\n.thh{\n    cursor: pointer;\n}\n.thh:hover{\n    font-weight: 900;\n}\n\n.date {\n    width: 7%\n}\n\n.city {\n    width: 15%\n}\n\na:hover {\n    color: #222;\n    font-weight: 700;\n    letter-spacing: -.5px\n}\n\n.head {\n    width: 100%;\n    text-align: center\n}\n\n.container {\n    width: 100%;\n    max-width: 980px;\n    margin: auto;\n    text-align: center;\n    display: table\n}\n\n.container table,\ntr,\ntd,\nth {\n    text-align: left;\n    border: solid 1px\n}\n\n@media screen and (min-width:1280px) {\n    .th {\n        font-size: 100%\n    }\n    .date {\n        width: 8%\n    }\n}\n\n@media screen and (max-width:745px) {\n    .th {\n        font-size: 75%;\n        width: 25%\n    }\n    .city {\n        width: 22%\n    }\n    .date {\n        width: 16%\n    }\n}",
+          code: "body,\nhtml {\n    font-family: 'Nunito', 'Proxima Nova Soft','Helvetica Neue',sans-serif\n}\na {\n    color: black;\n    text-decoration: none\n}\n.th {\n    font-size: 85%;\n    box-sizing: border-box;\n    word-wrap: break-word;\n    display: table-cell;\n    vertical-align: middle;\n    width: 25%;\n    border: solid 1px #EFEFEF;\n    page-break-before: auto;\n    page-break-after: auto;\n    page-break-inside: avoid\n}\n.thh{\n    cursor: pointer;\n}\n.thh:hover{\n    font-weight: 900;\n}\n\n.date {\n    width: 7%\n}\n\n.city {\n    width: 15%\n}\n\na:hover {\n    color: #222;\n    font-weight: 700;\n    letter-spacing: -.5px\n}\n\n.head {\n    width: 100%;\n    text-align: center\n}\n\n.container {\n    width: 100%;\n    max-width: 980px;\n    margin: auto;\n    text-align: center;\n    display: table\n}\n\n.container table,\ntr,\ntd,\nth {\n    text-align: left;\n    border: solid 1px\n}\n\n@media screen and (min-width:1280px) {\n    .th {\n        font-size: 100%\n    }\n    .date {\n        width: 8%\n    }\n}\n\n@media screen and (max-width:745px) {\n    .th {\n        font-size: 75%;\n        width: 25%\n    }\n    .city {\n        width: 22%\n    }\n    .date {\n        width: 16%\n    }\n}",
           virtualPath: "./index.marko.css",
           path: "./index.marko"
         }
