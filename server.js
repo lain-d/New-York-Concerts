131ddb7f6a7484538422d4789e8270d07cf2ee72
Dan Spagnoli <Dan.Spagnoli@vh1.com>
2017-12-04 14:22:25 -0500

    added other cities, not live yet till API limit removed

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 //Requirements and Config
 require("datejs");
 require("marko/node-require").install();
@@ -38,9 +39,38 @@ if (config.redisURL) {
 } else {
   console.log("no Redis attempting to get data from API");
   getData(0, data, config.zipCode, "concertCache", function(thedata) {
-    console.log("Saving Show Data in Memory");
+    console.log("NYC Saving Show Data in Memory");
     data = thedata;
+    console.log("Get Philly");
+    getData(0, phillydata, "19102", "phillyconcertCache", function(thedata2) {
+    console.log("Saving Show Data in Memory");
+    phillydata = thedata2;
+        getData(0, denverdata, "19102", "denverconcertCache", function(thedata3) {
+    console.log("Saving Show Data in Memory");
+    denverdata = thedata3;
+            getData(0, seattledata, "19102", "seattleconcertCache", function(thedata4) {
+    console.log("Saving Show Data in Memory");
+    seattledata = thedata4;
+                getData(0, chicagodata, "19102", "chicagoconcertCache", function(thedata5) {
+    console.log("Saving Show Data in Memory");
+    chicagodata = thedata5;
+     getData(0, ladata, "19102", "laconcertCache", function(thedata6) {
+    console.log("Saving Show Data in Memory");
+    ladata = thedata6;
+      getData(0, austindata, "19102", "austinconcertCache", function(thedata7) {
+    console.log("Saving Show Data in Memory");
+    austindata = thedata7;
+         getData(0, dcdata, "19102", "dcconcertCache", function(thedata8) {
+    console.log("Saving Show Data in Memory");
+    dcdata = thedata8;
   });
+  });
+  });
+  });
+  });
+  });
+  });
+});
 }
 
 //Update Data Script on the Hour
@@ -98,21 +128,76 @@ app.get("/phillydata", function(req, res) {
  
 });
 
-var phillyroute = express.Router();
- 
-//api specific routes 
-phillyroute.get('/', function(req, res) {
+var phillyr = express.Router();
+ //api specific routes 
+phillyr.get('/', function(req, res) {
      res.marko(indexTemplate, {
     events: phillydata,
     numevents: phillydata.length.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","),
     city: "Philly"
   });
 });
+var denverr = express.Router();
+ //api specific routes 
+denverr.get('/', function(req, res) {
+     res.marko(indexTemplate, {
+    events: denverdata,
+    numevents: denverdata.length.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+    city: "Denver"
+  });
+     });
+var chicagor = express.Router();
+ //api specific routes 
+chicagor.get('/', function(req, res) {
+     res.marko(indexTemplate, {
+    events: chicagodata,
+    numevents: chicagodata.length.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+    city: "Chicago"
+  });
+     });
+var lar = express.Router();
+ //api specific routes 
+lar.get('/', function(req, res) {
+     res.marko(indexTemplate, {
+    events: ladata,
+    numevents: ladata.length.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+    city: "LA"
+  });
+     });
+var dcr = express.Router();
+ //api specific routes 
+dcr.get('/', function(req, res) {
+     res.marko(indexTemplate, {
+    events: dcdata,
+    numevents: dcdata.length.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+    city: "DC"
+});});
+var austinr = express.Router();
+ //api specific routes 
+austinr.get('/', function(req, res) {
+     res.marko(indexTemplate, {
+    events: austindata,
+    numevents: austindata.length.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+    city: "Austin"
+});     });
+var seattler = express.Router();
+ //api specific routes 
+seattler.get('/', function(req, res) {
+     res.marko(indexTemplate, {
+    events: seattledata,
+    numevents: seattledata.length.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+    city: "Seattle"
+});    }); 
 
-app.use(subdomain('philly', phillyroute));
+app.use(subdomain('philly', phillyr));
+app.use(subdomain('denver', denverr));
+app.use(subdomain('chicago', chicagor));
+app.use(subdomain('dc', dcr));
+app.use(subdomain('la', lar));
+app.use(subdomain('seattle', seattler));
+app.use(subdomain('austin', austinr));
 
-
-//Main Route
+//Main Route for NYC
 app.get("/", function(req, res) {
   res.marko(indexTemplate, {
     events: data,
